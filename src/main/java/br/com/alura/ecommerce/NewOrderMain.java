8f24f617e7b7c89fdf7e3856498b972184cf6abe
antonio.marcos <antonio.malves.lima@gmail.com>
2020-11-25 15:20:29 -0300

    criacao de uma novo topico para email e criacao de um novo consumer para esse topico

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,13 +15,18 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "1334522,45633,2345456423";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null){
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("Sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCER_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+
     }
 
     private static Properties properties(){
